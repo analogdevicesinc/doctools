271d7c65518f6823301654b7f607f25fc19d03eb
Jorge Marques <jorge.marques@analog.com>
2024-09-22 21:15:24 -0300

    Stylesheet fixes, bump version
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/__init__.py
+++ adi_doctools/__init__.py
@@ -12,7 +12,7 @@ from .role import setup as role_setup
 from .lut import get_lut
 from .role.interref import interref_repos_apply
 
-__version__ = "0.3.43"
+__version__ = "0.3.44"
 
 logger = logging.getLogger(__name__)
 
