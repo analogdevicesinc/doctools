analogdevicesinc/doctools

commit d70ba6a93c5f7a47e06fc599e705187feef303c8

PDF build for documentation (#18)
- This method converts directly to PDF using the .rst files
(rst2pdf method)
- Build by running 'sphinx-build -b pdf' in the docs folder
and choose the output folder
- New parameter called ADOC_MEDIA_PRINT; This parameters is used to
signal the PDF export mode and can be either 1 or 0
- Tables export as they should now (without empty columns)
- Collapsible content is converted into tables
- Dropdowns which contain tables like the ones found in
'Generic AXI ADC' && 'Generic AXI DAC' wouldn't render
properly, but now they are fixed
- Fixed larger tables, usually appear under 'Configuration Parameters'
section, which contain a larger number of columns
- Added a new section called 'Exporting to PDF' where the PDF
export mode is explained
- Added in requirments.txt the three libraries needed to build
the PDF documentation

Signed-off-by: Cristian Mihai Popa <cristianmihai.popa@analog.com>

adi_doctools/directive/hdl.py
@@ -17,6 +17,7 @@ from ..parser.hdl import parse_hdl_regmap, resolve_hdl_regmap
 from ..parser.hdl import parse_hdl_build_status
 from ..writer.hdl_component import hdl_component
 
+
 log = {
     'signal': "{lib}/component.xml: Signal {signal} defined in the hdl-interfaces directive does not exist in the IP-XACT!",  # noqa: E501
     'path': "Inconsistent paths, {cwd} not in {doc}",
@@ -39,11 +40,11 @@ class directive_interfaces(directive_base):
         else:
             return string.replace("'MODELPARAM_VALUE.", '').replace("'", '')
 
-    def tables(self, subnode, content, component, lib_name):
+    def tables(self, subnode, content, component, lib_name, media_print = False):
         description = self.get_descriptions(content)
 
         if component is None:
-            subnode += self.generic_table(description)
+            subnode += self.generic_table(description, media_print = media_print)
             return subnode
 
         bs = component['bus_interface']
@@ -79,14 +80,17 @@ class directive_interfaces(directive_base):
 
             self.table_header(tgroup, ["Physical Port", "Logical Port", "Direction", "Dependency"])  # noqa: E501
 
+            # change type of tables entries when outputing to PDF
+            literal_ = 'literal' if not media_print else 'paragraph' 
+
             rows = []
             pm = bs[tag]['port_map']
             for key in pm:
                 self.column_entries(rows, [
-                    [key, 'literal'],
-                    [pm[key]['logical_port'], 'literal'],
+                    [key, literal_ ],
+                    [pm[key]['logical_port'], literal_],
                     [pm[key]['direction'], 'paragraph'],
-                    [self.pretty_dep(pm[key]['dependency'], key), 'literal'],
+                    [self.pretty_dep(pm[key]['dependency'], key), literal_],
                 ])
 
             tbody = nodes.tbody()
@@ -108,15 +112,18 @@ class directive_interfaces(directive_base):
 
         self.table_header(tgroup, ["Physical Port", "Direction", "Dependency", "Description"])  # noqa: E501
 
+        # change type of tables entries when outputing to PDF
+        literal_ = 'literal' if not media_print else 'paragraph'  
+
         rows = []
         pr = component['ports']
         dm = component['bus_domain']
         for key in pr:
             row = nodes.row()
-            self.column_entry(row, key, 'literal')
+            self.column_entry(row, key, literal_)
             self.column_entry(row, pr[key]['direction'], 'paragraph')
-            self.column_entry(row, self.pretty_dep(pr[key]['dependency'], key),
-                              'literal')
+            self.column_entry(row, self.pretty_dep(pr[key]['dependency'], key), literal_)
+            
             if 'clk' in key or 'clock' in key:
                 domain = 'clock domain'
             elif 'reset':
@@ -154,6 +161,8 @@ class directive_interfaces(directive_base):
     def run(self):
         env = self.state.document.settings.env
 
+        media_print = env.config.media_print
+
         node = node_div()
 
         if 'path' in self.options:
@@ -163,9 +172,9 @@ class directive_interfaces(directive_base):
 
         discover_hdl_component(env, lib_name)
         if lib_name in env.component:
-            self.tables(node, self.content, env.component[lib_name], lib_name)
+            self.tables(node, self.content, env.component[lib_name], lib_name, media_print)
         else:
-            self.tables(node, self.content, None, lib_name)
+            self.tables(node, self.content, None, lib_name, media_print)
 
         return [node]
 
@@ -187,7 +196,7 @@ class directive_regmap(directive_base):
 
         return (dword, byte)
 
-    def tables(self, subnode, obj, key):
+    def tables(self, subnode, obj, key, media_print = False):
         uid = "hdl-regmap-" + key
         section = nodes.section(ids=[uid])
 
@@ -202,14 +211,17 @@ class directive_regmap(directive_base):
         self.table_header(tgroup, ["DWORD", "BYTE", ["Reg Name", 3], "Description"])  # noqa: E501
         self.table_header(tgroup, [["", 1], "BITS", "Field Name", "Type", "Default Value", "Description"])  # noqa: E501
 
+        # change type of tables entries when outputing to PDF
+        literal_ = 'literal' if not media_print else 'paragraph' 
+
         rows = []
         for reg in obj['regmap']:
             dword, byte = self.get_hex_addr(reg['address'], reg['addr_incr'])
             self.column_entries(rows, [
-                [dword, 'literal', ['bold']],
-                [byte, 'literal', ['bold']],
-                [reg['name'], 'literal', ['bold'], 3],
-                [reg['description'], 'reST', ['description', 'bold']],
+                [dword, literal_, ['bold']],
+                [byte, literal_, ['bold']],
+                [reg['name'], literal_, ['bold'], 3],
+                [reg['description'], 'reST' if not media_print else 'paragraph', ['description', 'bold']],
             ], uid=uid)
 
             for field in reg['fields']:
@@ -234,12 +246,12 @@ class directive_regmap(directive_base):
                     bits = f"{bits[0]}:{bits[1]}"
 
                 self.column_entries(rows, [
-                    ["", 'literal', [''], 1],
-                    [f"[{bits}]", 'literal'],
-                    [field['name'], 'literal'],
-                    [field['rw'], 'literal'],
-                    [default, 'literal', ['default']],
-                    [field['description'], 'reST', ['description']],
+                    ["", literal_, [''], 1],
+                    [f"[{bits}]", literal_],
+                    [field['name'], literal_],
+                    [field['rw'], literal_],
+                    [default, 'default_value' if not media_print else 'paragraph', ['default']],
+                    [field['description'], 'reST' if not media_print else 'paragraph', ['description']],
                 ], uid=uid)
 
         tbody = nodes.tbody()
@@ -280,6 +292,8 @@ class directive_regmap(directive_base):
         env = self.state.document.settings.env
         owner = env.docname
 
+        media_print = env.config.media_print
+
         node = node_div()
 
         if 'name' in self.options:
@@ -303,7 +317,7 @@ class directive_regmap(directive_base):
 
         if owner not in env.regmaps[f]['owners']:
             env.regmaps[f]['owners'].append(owner)
-        self.tables(subnode, env.regmaps[f]['subregmap'][lib_name], lib_name)
+        self.tables(subnode, env.regmaps[f]['subregmap'][lib_name], lib_name, media_print)
 
         node += subnode
         return [node]
@@ -320,11 +334,11 @@ class directive_parameters(directive_base):
         else:
             return string
 
-    def tables(self, content, parameter, lib_name):
+    def tables(self, content, parameter, lib_name, media_print = False):
         description = self.get_descriptions(content)
 
         if parameter is None:
-            return self.generic_table(description)
+            return self.generic_table(description, media_print = media_print)
 
         tgroup = nodes.tgroup(cols=5)
         for _ in range(5):
@@ -334,11 +348,14 @@ class directive_parameters(directive_base):
         table += tgroup
 
         self.table_header(tgroup, ["Name", "Description", "Default Value", "Choices/Range"])  # noqa: E501
+        
+        # change type of tables entries when outputing to PDF
+        literal_ = 'literal' if not media_print else 'paragraph' 
 
         rows = []
         for key in parameter:
             row = nodes.row()
-            self.column_entry(row, "{:s}".format(key), 'literal')
+            self.column_entry(row, "{:s}".format(key), literal_)
             if key in description:
                 self.column_entry(row, description[key],
                                   'reST', classes=['description'])
@@ -348,7 +365,7 @@ class directive_parameters(directive_base):
                                   classes=['description'])
             for tag, ty in zip(['default'], ['literal']):
                 if parameter[key][tag] is not None:
-                    self.column_entry(row, parameter[key][tag], ty,
+                    self.column_entry(row, parameter[key][tag], ty if not media_print else 'paragraph',
                                       classes=[tag])
                 else:
                     logger.warning(f"Got empty {tag} at parameter {key}!")
@@ -375,7 +392,9 @@ class directive_parameters(directive_base):
     def run(self):
         env = self.state.document.settings.env
 
-        node = node_div()
+        media_print = env.config.media_print
+
+        node = node_div()        
 
         if 'path' not in self.options:
             self.options['path'] = env.docname.replace('/index', '')
@@ -386,9 +405,9 @@ class directive_parameters(directive_base):
         if lib_name in env.component:
             subnode += self.tables(self.content,
                                    env.component[lib_name]['parameters'],
-                                   lib_name)
+                                   lib_name, media_print)
         else:
-            subnode += self.tables(self.content, None, lib_name)
+            subnode += self.tables(self.content, None, lib_name, media_print)
 
         node += subnode
 

adi_doctools/__init__.py
@@ -63,7 +63,10 @@ def config_inited(app, config):
         except Exception as err:
             pass
         config.version = doc_version
-
+    # Parameter to enable PDF output tweaks
+    media_print = getenv("ADOC_MEDIA_PRINT", default="0")
+    media_print = True if media_print == "1" else False
+    config.media_print = media_print
 
 def builder_inited(app):
     if app.builder.format == 'html':

adi_doctools/directive/common.py
@@ -101,7 +101,7 @@ class directive_base(Directive):
                                   uid=uid)
         rows.append(row)
 
-    def generic_table(self, description, uid: Optional[str]=None):
+    def generic_table(self, description, uid: Optional[str]=None, media_print=False):
         tgroup = nodes.tgroup(cols=2)
         for _ in range(2):
             colspec = nodes.colspec(colwidth=1)
@@ -109,17 +109,24 @@ class directive_base(Directive):
         table = nodes.table()
         table += tgroup
 
+        # example: self.table_header(tgroup, ["Bear with me", "Description"])
         self.table_header(tgroup, ["Name", "Description"])
 
         rows = []
         for key in description:
             row = nodes.row()
+            
             entry = nodes.entry()
-            entry += nodes.literal(text="{:s}".format(key))
+            if not media_print:  # Check if not in PDF mode
+                entry += nodes.literal(text="{:s}".format(key))
+            else:
+                entry += nodes.paragraph(text="{:s}".format(key))  # Use paragraph for PDF mode
             row += entry
+            
             entry = nodes.entry()
             entry += parse_rst(self.state, description[key], uid=uid)
             row += entry
+            
             rows.append(row)
 
         tbody = nodes.tbody()
@@ -154,8 +161,7 @@ class directive_base(Directive):
 
         thead.append(row)
 
-    def collapsible(self, section,
-                    text: [str, Tuple[str, str]] = "", node=None):
+    def collapsible(self, section, text: [str, Tuple[str, str]] = "", node=None):
         """
         Creates a collapsible content.
         text: When a tuple, the first string is a unique id, useful when the

docs/conf.py
@@ -6,11 +6,16 @@ repository = 'doctools'
 project = 'Doctools'
 copyright = '2024, Analog Devices, Inc.'
 author = 'Analog Devices, Inc.'
+version = '0.3'
+
+locale_dirs = ['locales/']  # path is relative to the source directory
+language = 'en'
 
 # -- General configuration ---------------------------------------------------
 
 extensions = [
-    "adi_doctools",
+    'adi_doctools',
+    'rst2pdf.pdfbuilder'
 ]
 
 needs_extensions = {
@@ -23,6 +28,9 @@ source_suffix = '.rst'
 # -- Custom extensions configuration ------------------------------------------
 
 export_metadata = True
+
+#  -- Options for PDF output --------------------------------------------------
+# draft comment, future options for exporting to PDF
 
 # -- Options for HTML output --------------------------------------------------
 

commit 545172f43100012ee692be6e3019bfebaa872bd8

Fixup regression caused by d70ba6a
During this commit rebase, the author kept the removed option
'default_value'.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/directive/hdl.py
@@ -250,7 +250,7 @@ class directive_regmap(directive_base):
                     [f"[{bits}]", literal_],
                     [field['name'], literal_],
                     [field['rw'], literal_],
-                    [default, 'default_value' if not media_print else 'paragraph', ['default']],
+                    [default, literal_, ['default']],
                     [field['description'], 'reST' if not media_print else 'paragraph', ['description']],
                 ], uid=uid)
 