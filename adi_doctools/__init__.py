c95dbc5c60601ce518734e71d8dcaff69f2bfe9f
Jorge Marques <jorge.marques@analog.com>
2024-12-20 14:39:05 -0300

    __init__: Extend transform for unique ids
    Extend the transform step to make unique all ids, to resolve all
    collision in singlehtml mode.
    Use findall instead of deprecated traverse, added on Docutils 0.18 and
    requirement from Sphinx 7.1.2
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/__init__.py
+++ adi_doctools/__init__.py
@@ -132,15 +132,32 @@ class unique_ids(SphinxTransform):
             return
 
         def make_unique_id(node, id_):
+            """
+            A node contains multiple ids, the first is the title
+            text converted to ID, then, if present, the label
+            e.g.:
+              .. _documentation+a label:
+
+              My header
+              ---------
+
+            becomes:
+              ["my-header", "documentation-a-label"]
+
+            The first is the one is the one we worry about collision
+            """
             counter = 1
             id__ = id_
             while id__ in self.used_ids:
                 id__ = f"{id_}-{counter}"
                 counter += 1
             self.used_ids.add(id__)
-            node['ids'] = [id__]
+            node['ids'][0] = id__
 
-        for node in self.document.traverse(nodes.section):
+        # findall (Docutils >= 0.18.1) returns an iterator,
+        # and deprecates traverse (returns a list).
+        # Sphinx 7.1.2 requires Docutils >= 0.18.1
+        for node in self.document.findall():
             if 'ids' in node and node['ids']:
                 make_unique_id(node, node['ids'][0])
 
