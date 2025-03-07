446289a22c452ea7cff0f76d06e47760701e2806
Jorge Marques <jorge.marques@analog.com>
2025-03-07 21:19:01 +0100

    ci: enforce python version
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/install-python.sh
+++ ci/install-python.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# for: build-doc-min
+# for: build-doc-*
 
 set -e
 
@@ -7,9 +7,9 @@ zypper install -y --no-recommends \
     gcc awk libffi-devel
 
 PYTHON_MIN=$1
-PYTHON_MIN_SHA=$2
+PYTHON_MIN_SHA=$3
 curl -o python.tar.xz -L https://www.python.org/ftp/python/$PYTHON_MIN/Python-$PYTHON_MIN.tar.xz
-echo "$PYTHON_MIN_SHA python.tar.xz" | sha3sum -a 256 -c
+echo "$PYTHON_MIN_SHA python.tar.xz" | md5sum -c
 
 tar xf python.tar.xz
 cd Python-$PYTHON_MIN
@@ -22,6 +22,7 @@ cd Python-$PYTHON_MIN
 make -s -j5
 make install
 
-ln -s /opt/python/$1/bin/python3.8 /usr/local/bin/python3.8
+ln -s /opt/python/$1/bin/python$2 /usr/local/bin/python$2
+ln -s /opt/python/$1/bin/pip$2 /usr/local/bin/pip$2
 echo "/opt/python/$1/lib" >> /etc/ld.so.conf.d/python$1.conf
 ldconfig
