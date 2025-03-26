c6c950bb13284414d1ebc870b4781e8b8fb3896d
Jorge Marques <jorge.marques@analog.com>
2025-03-26 10:57:01 +0100

    ci: Add aux scripts
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

---
+++ entrypoint.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+runner_version=v1
+github_token=$(cat /run/secrets/github_token 2> /dev/null)
+org_repository=$(cat /run/secrets/org_repository 2> /dev/null)
+
+source /usr/local/bin/github-api.sh
+
+if [[ -z $org_repository ]]; then
+    echo "No org_repository provided"
+    exit 1
+fi
+
+function get_runner_token () {
+    if [[ ! -z $github_token ]]; then
+        runner_token=$(
+            gh-actions-token $github_token \
+                             $org_repository \
+                             runner_token
+        )
+
+        if [[ "$runner_token" == "null" ]]; then
+            echo "Failed to get '$org_repository' runner_token, check github_token permission"
+            exit 1
+        fi
+    else
+        runner_token=$(cat /run/secrets/runner_token 2> /dev/null)
+
+        if [[ -z $runner_token ]]; then
+            echo "No runner_token or github_token provided"
+            exit 1
+        fi
+    fi
+}
+
+get_runner_token
+
+if [[ ! -z $runner_labels ]]; then
+    runner_version+="-$runner_labels"
+fi
+
+name=$(echo $org_repository | sed 's|/|-|g')-$(echo $runner_token | sha3sum -a 256 | head -c4)-$(tr -dc A-Za-z0-9 </dev/urandom | head -c2; echo)
+
+set -e
+
+/home/runner/actions-runner/config.sh \
+    --url https://github.com/$org_repository \
+    --token $runner_token \
+    --labels "$runner_version" \
+    --name  $name
+
+function cleanup () {
+    get_runner_token
+
+    /home/runner/actions-runner/config.sh remove \
+        --token $runner_token
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+/home/runner/actions-runner/run.sh & wait $!
---
+++ github-api.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+gh-get-release-id()
+{
+  release_id=$(curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$2/releases/tags/$3" \
+    | jq -r .id)
+  echo "$4=$release_id" >> "$GITHUB_ENV"
+  echo $release_id
+}
+
+gh-get-asset-id()
+{
+  asset_id=$(curl -L \
+    -H "Accept: application/vnd.github+json"  \
+    -H "Authorization: Bearer $1"  \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$2/releases/$3/assets" \
+    | jq -r ".[] | select(.name==\"$asset_name\") | .id")
+  echo "$4=$asset_id" >> "$GITHUB_ENV"
+  echo $asset_id
+}
+
+gh-create-release()
+{
+  release_id=$(curl -L \
+   -X POST \
+   -H "Accept: application/vnd.github+json" \
+   -H "Authorization: Bearer $1" \
+   -H "X-GitHub-Api-Version: 2022-11-28" \
+   "https://api.github.com/repos/$2/releases" \
+   -d "{\"tag_name\":\"$3\",\"name\":\"$3\",\"make_latest\":\"true\"}" \
+   | jq -r .id)
+  echo "$4=$release_id" >> "$GITHUB_ENV"
+  echo $release_id
+}
+
+gh-upload-asset()
+{
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -H "Content-Type: application/octet-stream" \
+    "https://uploads.github.com/repos/$2/releases/$3/assets?name=$4" \
+    --data-binary "@$5"
+}
+
+gh-delete-asset()
+{
+  curl -L \
+    -X DELETE \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$2/releases/assets/$3"
+}
+
+gh-update-commitish()
+{
+  curl -s \
+    -X PATCH \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    -d '{"target_commitish": "$3"}' \
+    "https://api.github.com/repos/$2/releases/$4"
+}
+
+gh-actions-token()
+{
+  runner_token=$(curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$2/actions/runners/registration-token" \
+    | jq -r .token)
+  echo $runner_token
+}
+
+gh-get-number-commits()
+{
+  total_commits=$(curl -L \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$2/compare/$3...$4" \
+    | jq ".total_commits")
+  echo "total_commits=$total_commits" >> "$GITHUB_ENV"
+  echo $total_commits
+}
+
+gh-cancel-workflow()
+{
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    https://api.github.com/repos/$2/actions/runs/$3/cancel
+}
