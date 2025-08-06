d6d3d6786949caceb60e569f834beba46a63b393
Jorge Marques <jorge.marques@analog.com>
2025-08-06 21:08:43 +0200

    hot_reload: hide all tocwrapper
    To also hide 'On this page', and any other future content.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/theme/cosmic/scripts/hot_reload.js
+++ adi_doctools/theme/cosmic/scripts/hot_reload.js
@@ -15,6 +15,7 @@ export class HotReload {
     $.bodywrapper = new DOM(DOM.get('.documentwrapper .bodywrapper'))
     $.content = new DOM(DOM.get('.documentwrapper .body'))
     $.localtoc = new DOM(DOM.get('.localtoc nav'))
+    $.tocwrapper = new DOM(DOM.get('.localtoc .tocwrapper'))
     $.related = new DOM(DOM.get('.documentwrapper .related'))
     $.breadcrumb = DOM.get('.bodywrapper .body-header .breadcrumb')
 
@@ -102,7 +103,7 @@ export class HotReload {
     this.parent.content_actions.init()
 
     this.$.bodywrapper.classList.remove('fetch')
-    this.$.localtoc.classList.remove('fetch')
+    this.$.tocwrapper.classList.remove('fetch')
     this.$.loader.classList.remove('fetch')
 
     if (url.hash)
@@ -129,7 +130,7 @@ export class HotReload {
     if (new_state)
       history.pushState(request_url.href, '', request_url.href)
 
-    this.$.localtoc.classList.add('fetch')
+    this.$.tocwrapper.classList.add('fetch')
     this.$.bodywrapper.classList.add('fetch')
     const loader_= setTimeout(() => {
       this.$.loader.classList.add('fetch')
