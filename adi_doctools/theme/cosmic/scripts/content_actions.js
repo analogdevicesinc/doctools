ecd7bbbd55d5e8e360d9c6656f2b1b99b0644d17
Jorge Marques <jorge.marques@analog.com>
2025-08-20 19:11:01 +0200

    content_actions: add collections to the sidebar
    Show a simple dropdowns with each collection the current page belong to.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/theme/cosmic/scripts/content_actions.js
+++ adi_doctools/theme/cosmic/scripts/content_actions.js
@@ -6,12 +6,18 @@ import {Toolbox} from './toolbox.js'
  * Enhance content interactions.
  * The actions are:
  * * Copy button on code and shell blocks.
+ * * Collections, groups of pages of the same category.
  */
 export class ContentActions {
   constructor (app) {
     this.$ = {}
     this.parent = app
-    this.init()
+    this.collection
+
+    if (typeof this.parent.fetch === 'object')
+      this.parent.fetch.then(this.init.bind(this))
+    else
+      this.init()
 
     app.content_actions = this
   }
@@ -41,12 +47,144 @@ export class ContentActions {
       item.insertAdjacentElement('afterend', button.$)
     })
   }
+  init_collection_wrapper () {
+    let tocwrapper = new DOM(DOM.get('.localtoc .tocwrapper'))
+    let header = new DOM('div', {
+      'className': 'header'
+    })
+    header.innerText = "Collections"
+    this.$.collection = new DOM('div',
+      {'className': 'collections'}
+    ).append(header)
+
+    tocwrapper.append(this.$.collection)
+  }
+  auto_collection_name (repo, path) {
+    const repo_name = repo in  this.parent.state.metadata.repotoc ?
+      this.parent.state.metadata.repotoc[repo].name : repo
+    if (this.collection_pattern === undefined ||
+       !(repo in this.collection_pattern))
+      return `${repo_name} ${path}`
+
+    for (const pattern in this.collection_pattern[repo]) {
+      const regex = new RegExp(pattern)
+      if (regex.test(path))
+        return this.collection_pattern[repo][pattern]
+          .replace("${repository}", repo_name)
+          .replace("${path}", path)
+    }
+
+    return `${repo_name} ${path}`
+  }
+  add_collection_entry (ul, repo, entry, props) {
+    const name = "name" in props ? props["name"] : this.auto_collection_name(repo, entry)
+    const url = new URL(`${this.parent.state.metadata.remote_doc}${repo}/${entry}`)
+    const m = new DOM('a', {'href': url, 'target': 'blank'})
+    m.innerText = name
+    ul.append(new DOM('li').append(m))
+  }
+  add_collection (name, obj) {
+    if (!this.$.collection)
+      this.init_collection_wrapper()
+
+    const id = name.replace(" ", "-")
+    let collection = new DOM('label', {
+      'className': 'sub-header',
+      'htmlFor': `collection-${id}`,
+    })
+    let input_collection = new DOM('input', {
+      'className': 'collection-collapse',
+      'type': 'checkbox',
+      'id': `collection-${id}`,
+    })
+    input_collection.$.checked = true
+    collection.innerText = name
+
+    let ul = new DOM('ul')
+    for (const repo in obj) {
+      for (const page in obj[repo])
+        this.add_collection_entry(ul, repo, page, obj[repo][page])
+    }
+    this.$.collection.append([
+      new DOM('div', {'className': 'group'}).append([
+        input_collection,
+        collection,
+        new DOM('span').append(ul)
+      ]),
+    ])
+  }
+  load_collection () {
+    const repo = this.parent.state.repository
+    const obj = this.collection
+    if (!("collection" in obj))
+      return
+    if ("pattern" in obj)
+      this.collection_pattern = obj["pattern"]
+    let path = ""
+    if (this.parent.state.offline)
+      path = new URL("file://"+location.pathname).href
+    else
+      path = new URL(location.pathname, location.origin).href
+    let base = new URL(this.parent.state.content_root, path).href
+
+    for (const key in obj["collection"]) {
+      if (!("include" in obj["collection"][key]) ||
+          !(repo in obj["collection"][key]['include']))
+        continue
+
+      const pages = obj["collection"][key]['include'][repo]
+      for (const page in pages) {
+        const collection_base = new URL(page, base).href
+        if (path.startsWith(collection_base)) {
+          this.add_collection(key, obj["collection"][key]['include'])
+          break
+        }
+      }
+    }
+  }
+  deinit_collection () {
+    this.collection_controller.abort()
+
+    if (!this.$.collection)
+      return
+
+    this.$.collection.$.remove()
+    delete this.$.collection
+    delete this.collection_pattern
+  }
+  init_collection () {
+    this.collection_controller = new AbortController();
+    const signal = this.collection_controller.signal
+
+    let state = this.parent.state
+    const base_url = (state.subhost === '' || state.offline === true) ?
+      new URL(`${state.metadata.remote_doc}/documentation`) :
+      new URL('documentation', new URL(state.subhost, location.origin))
+    // Do you want to use single_hosted with same origin? use below:
+    //const base_url =
+    //  new URL(new URL(state.subhost, location.origin))
+
+    if (this.collection) {
+      this.load_collection()
+      return
+    }
+    const response = fetch(
+      new Request(new URL('collection.json', base_url+'/')),
+      { signal }
+    )
+      .then(response => response)
+      .then(response => response.json())
+      .then(obj => {this.collection = obj ; this.load_collection()})
+      .catch(err => {})
+  }
   deinit () {
+    this.deinit_collection()
   }
   init () {
     if (!document.querySelector('script[src*="_static/copybutton.js"]'))
       this.copy_button()
     else
       console.log('copy_button: sphinx-copy-button extension is present, skipping')
+    this.init_collection()
   }
 }
