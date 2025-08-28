f2e10f3922638d29ebe5a02a15cd4316545162fb
Jorge Marques <jorge.marques@analog.com>
2025-08-28 17:00:34 +0200

    ci: drop github-api.sh from container
    Use the method directly in the entrypoint.sh
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/entrypoint.sh
+++ ci/entrypoint.sh
@@ -31,10 +31,13 @@ fi
 
 function get_runner_token () {
     if [[ ! -z "$github_token_" ]]; then
-        runner_token_=$(
-            gh-actions-token $github_token_ \
-                             $org_repository_
-        )
+        runner_token_=$(curl -L \
+          -X POST \
+          -H "Accept: application/vnd.github+json" \
+          -H "Authorization: Bearer $github_token_" \
+          -H "X-GitHub-Api-Version: 2022-11-28" \
+          "https://api.github.com/repos/$org_repository_/actions/runners/registration-token" \
+          | jq -r .token)
 
         if [[ "$runner_token_" == "null" ]]; then
             echo "Failed to get '$org_repository_' runner_token, check github_token permission"
--- ci/github-api.sh
+++ ci/github-api.sh
@@ -71,18 +71,6 @@ gh-update-commitish()
     "https://api.github.com/repos/$2/releases/$4"
 }
 
-gh-actions-token()
-{
-  runner_token=$(curl -L \
-    -X POST \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer $1" \
-    -H "X-GitHub-Api-Version: 2022-11-28" \
-    "https://api.github.com/repos/$2/actions/runners/registration-token" \
-    | jq -r .token)
-  echo $runner_token
-}
-
 gh-get-number-commits()
 {
   total_commits=$(curl -L \
