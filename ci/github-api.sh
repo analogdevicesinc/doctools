ace69648d04af99a82e523dd49ef87f9dd380c6e
Jorge Marques <jorge.marques@analog.com>
2025-08-28 19:30:45 +0200

    .github: use api to create tag, do not dirty on new tag
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/github-api.sh
+++ ci/github-api.sh
@@ -24,6 +24,20 @@ gh-get-asset-id()
   echo $asset_id
 }
 
+gh-create-tag()
+{
+  curl -L \
+    -X POST \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer $1" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    "https://api.github.com/repos/$2/git/refs" \
+    -d "{
+      \"ref\": \"refs/tags/$3\",
+      \"sha\": \"$4\"
+    }"
+}
+
 gh-create-release()
 {
   release_id=$(curl -L \
