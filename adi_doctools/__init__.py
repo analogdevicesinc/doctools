9de7ea899abc000d14d955d3a2d9414e2f3d2f77
Jorge Marques <jorge.marques@analog.com>
2025-09-01 14:14:15 +0200

    version: release v0.4.20
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/__init__.py
+++ adi_doctools/__init__.py
@@ -16,7 +16,7 @@ from .lut import get_lut
 from .role.interref import interref_repos_apply, interref_repos_assert
 from .monkeypatch import monkeypatch_figure_numbers
 
-__version__ = "0.4.19"
+__version__ = "0.4.20"
 
 
 def get_navigation_tree(app, context, pagename):
