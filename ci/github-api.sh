f9671847b5291129a756a3f5743e848ab72d7459
Jorge Marques <jorge.marques@analog.com>
2025-09-09 17:09:59 +0200

    ci: fixup github-api@gh-update-commitish
    Improper escaped quotes
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/github-api.sh
+++ ci/github-api.sh
@@ -81,7 +81,7 @@ gh-update-commitish()
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: Bearer $1" \
     -H "X-GitHub-Api-Version: 2022-11-28" \
-    -d '{"target_commitish": "$3"}' \
+    -d "{\"target_commitish\": \"$3\"}" \
     "https://api.github.com/repos/$2/releases/$4"
 }
 
