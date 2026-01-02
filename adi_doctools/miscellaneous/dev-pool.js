70c041f25c5c7503d374b8bae65d048e584d9cd4
Jorge Marques <jorge.marques@analog.com>
2026-01-02 18:11:39 +0100

    dev-pool: add visual status
    Add a fixed element in the bottom right corner to indicate the pooling
    reload (looks for changes): "Watching changes", "Degraded still watching",
    or "Not watching changes".
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/miscellaneous/dev-pool.js
+++ adi_doctools/miscellaneous/dev-pool.js
@@ -30,6 +30,11 @@ window.onbeforeunload = () => {
  * Pool changes, return a text on success and true on error.
  */
 class PoolChanges {
+  constructor () {
+    let $ = this.$ = {}
+
+    this.status_dom()
+  }
   /**
    * Do a Fetch request, the sent object is implicitly converted to text.
    * @param {string} url - URL to file.
@@ -67,7 +72,7 @@ class PoolChanges {
   /**
    * Find .dev-pool, on first match, return url and content.
    */
-  static async search (urls) {
+  async search (urls) {
     if ('file:' == window.location.protocol)
       return [null, null]
 
@@ -98,15 +103,123 @@ class PoolChanges {
 
     return [null, null]
   }
+  /**
+   * Add visual feedback.
+   */
+  status_dom () {
+    let elem = document.createElement('div')
+    elem.id = "dev_pool_status"
+    document.body.append(elem)
+
+    let style = document.createElement('style')
+    document.head.appendChild(style)
+
+    let sheet = style.sheet
+
+    sheet.insertRule(`
+      @keyframes pulse {
+        0% {
+          transform: scale(1);
+          opacity: .8;
+        }
+        50% {
+          transform: scale(.75);
+          opacity: .5;
+        }
+        100% {
+          transform: scale(1);
+          opacity: .8;
+        }
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status {
+        align-items: center;
+        position: fixed;
+        display: flex;
+        bottom: .75em;
+        right: .75em;
+        z-index: 20;
+        background-color: rgba(125, 125, 125, 0.2);
+        border: 1px solid rgba(125, 125, 125, 1);
+        border-radius: 1em;
+        padding: .25em .5em .25em .25em;
+        backdrop-filter: blur(1em);
+      }`, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status:before {
+        width: 1em;
+        height: 1em;
+        display: block;
+        content: '';
+        border-radius: 1em;
+        border: 1px solid rgba(125, 125, 125, .5);
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status.waiting_changes:before {
+        animation: pulse 2s infinite;
+        background: #92befc;
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status.waiting_changes.degraded:before {
+        background: #ae4410;
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status.disconnected:before {
+        background: #c81a28;
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status:after {
+        font-size: .8em;
+        font-weight: bold;
+        margin-left: .25em;
+        display: block;
+        content: '...';
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status.waiting_changes:after {
+        content: 'Watching changes';
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status.waiting_changes.degraded:after {
+        content: 'Degraded, still watching';
+      }
+    `, 0)
+
+    sheet.insertRule(`
+      #dev_pool_status.disconnected:after {
+        content: 'Not watching changes';
+      }
+    `, 0)
+
+    this.$.state = elem
+  }
 }
+
+let pool_changes = new PoolChanges()
 /*
  * Serve watch mode, reload webpage on changes.
  * Alternative to selenium.
  */
-PoolChanges.search(PoolChanges.get_paths()).then(obj => {
+pool_changes.search(PoolChanges.get_paths()).then(obj => {
   const [url, obj_] = obj
   if (!url) {
     console.log("File .dev-pool is absent, pooling interface disabled.")
+    pool_changes.$.state.classList.add('disconnected')
     return
   }
   do_pool = () => {
@@ -121,13 +234,17 @@ PoolChanges.search(PoolChanges.get_paths()).then(obj => {
         else
           location.reload()
       }
+      pool_changes.$.state.classList.remove('degraded')
       setTimeout(do_pool, 500)
     }).catch(e => {
       if (e.statusText === "File not found") {
         console.error(`${e.statusText}: File ${url} was removed, pooling interface disabled.`)
+        pool_changes.$.state.classList.remove('waiting_changes')
+        pool_changes.$.state.classList.add('disconnected')
       } else {
         if (e.name !== last_error_name) {
           console.log(`${e.name}: Failed to fetch ${url}, but still trying.`)
+          pool_changes.$.state.classList.add('degraded')
           last_error_name = e.name
         }
         setTimeout(do_pool, 500)
@@ -136,6 +253,7 @@ PoolChanges.search(PoolChanges.get_paths()).then(obj => {
   }
 
   console.log(`File ${url} is present, pooling interface enabled.`)
+  pool_changes.$.state.classList.add('waiting_changes')
   pool_timestamp = Number(obj_.split("\n")[0])
   setTimeout(do_pool, 500)
 })
