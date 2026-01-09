5714dbd7cee105a375af5c719a066cbbd7cd2de3
Jorge Marques <jorge.marques@analog.com>
2026-01-09 16:47:05 +0100

    cloudsmith-api: add cs-package-store-raw
    Uploads raw packages
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/cloudsmith-api.sh
+++ ci/cloudsmith-api.sh
@@ -34,3 +34,17 @@ cs-package-store-docker()
          \"tags\": \"$5\"}" \
     https://api.cloudsmith.com/v1/packages/$2/$3/upload/docker/
 }
+
+cs-package-store-raw () {
+  curl -sfL \
+    -X POST \
+    -H "Accept: application/json" \
+    -H "Content-Type: application/json" \
+    -H "X-Api-Key: $1" \
+    -d "{\"package_file\":\"$4\", \
+         \"republish\":\"true\", \
+         \"name\":\"$5\", \
+         \"tags\": \"$6\", \
+         \"version\":\"$7\"}" \
+    https://api.cloudsmith.com/v1/packages/$2/$3/upload/raw/
+}
