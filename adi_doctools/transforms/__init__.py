775644ef5d3da6daaffd3f41bd042cb0cc7a9be2
Jorge Marques <jorge.marques@analog.com>
2026-01-19 17:39:14 +0100

    latex: add support to shell directive
    Reformat into merged nodes to generate fewer sphinxVerbatim.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/transforms/__init__.py
+++ adi_doctools/transforms/__init__.py
@@ -1,9 +1,11 @@
 from .linkcheck_sitemap import setup as linkcheck_sitemap_setup
 from .aggregate import setup as aggregate_setup
 from .lfs import lfs_setup
+from .latex_shell import setup as latex_shell_setup
 
 setup = [
     linkcheck_sitemap_setup,
     aggregate_setup,
     lfs_setup,
+    latex_shell_setup,
 ]
