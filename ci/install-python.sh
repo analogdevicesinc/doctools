801d726ac154ff49a0b82426dcd6805ae0d3c5fa
Jorge Marques <jorge.marques@analog.com>
2026-01-26 17:54:14 +0100

    .github: bump required to v2
    No system pip, must use ensurepip after venv creation.
    python3 means latest installed python3.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/install-python.sh
+++ ci/install-python.sh
@@ -16,16 +16,15 @@ cd Python-$PYTHON_VERSION
 ./configure \
     --prefix=/opt/python/$1 \
     --enable-shared \
-    --enable-optimizations
+    --enable-optimizations \
+    --with-ensurepip=no
 
 make -s -j5
 make install
 
 ln -s "/opt/python/$1/bin/python$2" "/usr/local/bin/python$2"
-ln -s "/opt/python/$1/bin/pip$2" "/usr/local/bin/pip$2"
 echo "/opt/python/$1/lib" >> /etc/ld.so.conf.d/python$1.conf
 ldconfig
-/opt/python/$1/bin/python$2 -m pip install --upgrade pip
 
 cd ..
 rm -r Python-$PYTHON_VERSION python.tar.xz
