224766c31659b492e8693fb9cfcdd5aec4a178c0
Jorge Marques <jorge.marques@analog.com>
2026-01-26 17:16:35 +0100

    toctree_preview: revert to doctree resolved
    Does not require monkeypatch, being less likely to break. The sphinx
    step uses on findall(addnodes.toctree) to resolve unresolved toctrees;
    the node is early replaced by the resolved list.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- adi_doctools/directive/toctree_preview.py
+++ adi_doctools/directive/toctree_preview.py
@@ -2,25 +2,25 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import warnings
 from docutils import nodes
 from sphinx import addnodes
 from sphinx.directives.other import TocTree
-from sphinx.util.docutils import LoggingReporter
 from sphinx.util import logging
-from sphinx.environment import BuildEnvironment
-from sphinx.environment.adapters import toctree as toctree_adapters
-from sphinx.deprecation import RemovedInSphinx11Warning
+from .common import get_document_description
 
 if TYPE_CHECKING:
     from docutils.nodes import Node
     from sphinx.application import Sphinx
-    from sphinx.builders import Builder
 
 logger = logging.getLogger(__name__)
 
 
 class directive_toctree_preview(TocTree):
+    """
+    Creates a toctree, but adds a preview of the page.
+    Note that it won't show sub-levels, only depth 1.
+    """
+
     def run(self) -> list[Node]:
         result = super().run()
 
@@ -31,126 +31,45 @@ class directive_toctree_preview(TocTree):
 
         return result
 
-def toctree_preview_setup(app: Sphinx) -> None:
-    original_get_and_resolve = BuildEnvironment.get_and_resolve_doctree
+def directive_toctree_preview_doctree_resolved(app, doctree, fromdocname):
+    for toctreenode in doctree.findall(addnodes.toctree):
+        if not toctreenode.get('include_description', False):
+            continue
 
-    def resolve_toctree_preview(
-        env: BuildEnvironment,
-        docname: str,
-        builder: Builder,
-        toctree: addnodes.toctree,
-        *,
-        prune: bool = True,
-        maxdepth: int = 0,
-        titles_only: bool = False,
-        collapse: bool = False,
-        includehidden: bool = False,
-        tags,
-        ):
-        """
-        Alternative to _resolve_toctree with previews.
-        Limitation: only displays the first depth.
-        """
         bullet_list = nodes.bullet_list()
         bullet_list['classes'].append('toctree-preview')
 
-        for title_, ref in toctree['entries']:
-            title = env.titles[ref].astext()
+        for title_, docname in toctreenode['entries']:
+            title = app.env.titles[docname].astext()
 
-            ref_doctree = original_get_and_resolve(
-                env, ref, builder, tags=tags, prune_toctrees=prune
+            doctree = app.env.get_doctree(docname)
+            # doctree_resolved may not have been yield for doctree yet,
+            # so do the same call as the directive_description_doctree_resolved
+            description = get_document_description(doctree)
+
+            node_list_item = nodes.list_item()
+            node_title = nodes.paragraph()
+
+            ref = nodes.reference('', '', internal=True)
+            ref['refuri'] = app.builder.get_relative_uri(
+                app.env.docname, docname
             )
-            description = None
-            for node in ref_doctree.findall(nodes.meta):
-                if node['name'] == 'description':
-                    description = node['content']
-                    break
+            ref += nodes.Text(title)
 
-            list_item = nodes.list_item()
-            title_para = nodes.paragraph()
-
-            reference = nodes.reference('', '', internal=True)
-            reference['refuri'] = builder.get_relative_uri(docname, ref)
-            reference['anchorname'] = ''
-            reference += nodes.Text(title)
-
-            title_para += reference
-            list_item += title_para
+            node_title += ref
+            node_list_item += node_title
 
             if description:
-                desc_para = nodes.paragraph(text=description)
-                list_item += desc_para
+                node_description = nodes.paragraph(text=description)
+                node_list_item += node_description
 
-            bullet_list += list_item
+            bullet_list += node_list_item
 
-        toctree.replace_self(bullet_list)
-        return
+        # Replace pending resolve_toctree with early-resolved
+        toctreenode.replace_self(bullet_list)
 
-    def get_and_resolve_doctree_(
-        self,
-        docname: str,
-        builder: Builder,
-        *,
-        tags,
-        doctree: nodes.document | None = None,
-        prune_toctrees: bool = True,
-        includehidden: bool = False,
-    ) -> nodes.document:
-        """Read the doctree from the pickle, resolve cross-references and
-        toctrees and return it.
-        Adapted from: sphinx/enviroment/__init__.py@get_and_resolve_doctree
-        """
-        if tags is ...:
-            warnings.warn(
-                "'tags' will become a required keyword argument "
-                'for global_toctree_for_doc() in Sphinx 11.0.',
-                RemovedInSphinx11Warning,
-                stacklevel=2,
-            )
-            tags = builder.tags
 
-        if doctree is None:
-            try:
-                doctree = self._write_doc_doctree_cache.pop(docname)
-                doctree.settings.env = self
-                doctree.reporter = LoggingReporter(str(self.doc2path(docname)))
-            except KeyError:
-                doctree = self.get_doctree(docname)
-
-        # resolve all pending cross-references
-        self.apply_post_transforms(doctree, docname)
-
-        # now, resolve all toctree nodes
-        for toctreenode in doctree.findall(addnodes.toctree):
-            # monkeypatch-ed this call
-            if toctreenode.get('include_description', False):
-                resolve_toctree_preview(
-                    self,
-                    docname,
-                    builder,
-                    toctreenode,
-                    prune=prune_toctrees,
-                    includehidden=includehidden,
-                    tags=tags,
-                )
-                continue
-
-            result = toctree_adapters._resolve_toctree(
-                self,
-                docname,
-                builder,
-                toctreenode,
-                prune=prune_toctrees,
-                includehidden=includehidden,
-                tags=tags,
-            )
-            if result is None:
-                toctreenode.parent.replace(toctreenode, [])
-            else:
-                toctreenode.replace_self(result)
-
-        return doctree
-
-    BuildEnvironment.get_and_resolve_doctree = get_and_resolve_doctree_
-
+def toctree_preview_setup(app: Sphinx) -> None:
     app.add_directive('toctree-preview', directive_toctree_preview)
+
+    app.connect('doctree-resolved', directive_toctree_preview_doctree_resolved)
