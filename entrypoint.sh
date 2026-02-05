d6a9a5f4f699ab7a0dc5be7cf6227149950e979a
Jorge Marques <jorge.marques@analog.com>
2026-02-05 16:10:37 +0100

    entrypoint: add support for github location
    GitHub base image puts config.sh at /home/runner, add support to this
    location, falling-back to old path. Intended for compatibility with
    ARC.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,7 +67,9 @@ name=$(echo $org_repository_ | sed 's|/|-|g')-$version_-$name_label_
 
 set -e
 
-(cd /home/runner/actions-runner ; ./config.sh \
+[[ -x /home/runner/config.sh ]] && runner_dir="/home/runner" || runner_dir="/home/runner/actions-runner"
+readonly runner_dir
+(cd "$runner_dir" ; ./config.sh \
     --url https://github.com/$org_repository_ \
     --token $runner_token_ \
     --labels "$runner_labels_" \
@@ -79,11 +81,11 @@ set -e
 function cleanup () {
     get_runner_token
 
-    (cd /home/runner/actions-runner ; ./config.sh remove \
+    (cd "$runner_dir" ; ./config.sh remove \
         --token $runner_token_)
 }
 
 trap 'cleanup; exit 130' INT
 trap 'cleanup; exit 143' TERM
 
-/home/runner/actions-runner/run.sh & wait $!
+$runner_dir/run.sh & wait $!
