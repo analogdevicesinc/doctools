analogdevicesinc/doctools

commit 6de034c0b486df364d8ce1e618400b3be34c2511

bashrc: add _ci fallback
For older name convention.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

bashrc
@@ -6,7 +6,8 @@ if git rev-parse --is-inside-work-tree  > /dev/null 2>&1 ; then
 	pushd $(realpath $(git rev-parse --git-common-dir)/..) 1>/dev/null
 
 	ci_worktree=$(git worktree list | grep '\[ci\]' | cut -f1 -d " ")
-	if [[ ! -z "$ci_worktree" ]]; then
+	[[ -n "$ci_worktree" ]] && ci_worktree=$(git worktree list | grep '\[_ci\]' | cut -f1 -d " ")
+	if [[ -n "$ci_worktree" ]]; then
 		export CI_WORKTREE=$ci_worktree
 		if [[ -f "$ci_worktree/user.sh" ]]; then
 			source $ci_worktree/ci/user.sh

commit b290ee866de4bb39dd74276dd2664a001c9ddab4

fix: bashrc: empty ci_worktree
The logic was inverted, causing to clear ci_worktree for 'ci' worktree.
Fixes: 6de034c0b486 ("bashrc: add _ci fallback")

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

bashrc
@@ -6,7 +6,7 @@ if git rev-parse --is-inside-work-tree  > /dev/null 2>&1 ; then
 	pushd $(realpath $(git rev-parse --git-common-dir)/..) 1>/dev/null
 
 	ci_worktree=$(git worktree list | grep '\[ci\]' | cut -f1 -d " ")
-	[[ -n "$ci_worktree" ]] && ci_worktree=$(git worktree list | grep '\[_ci\]' | cut -f1 -d " ")
+	[[ -z "$ci_worktree" ]] && ci_worktree=$(git worktree list | grep '\[_ci\]' | cut -f1 -d " ")
 	if [[ -n "$ci_worktree" ]]; then
 		export CI_WORKTREE=$ci_worktree
 		if [[ -f "$ci_worktree/user.sh" ]]; then