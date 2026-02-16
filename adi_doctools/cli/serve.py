analogdevicesinc/doctools

commit 8570e238eff190c9452af7002f2f5bee87065d3c

serve: explicitely track conf.py
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/cli/serve.py
@@ -523,6 +523,9 @@ def serve(directory, port, dev, selenium, once, builder):
             files.extend(__files)
             for f in __files:
                 ctime.append(stat(f).st_mtime)
+        if path.isfile(f):
+            files.append(conf_py)
+            ctime.append(stat(conf_py).st_mtime)
         dirs = [d for d in listdir(sourcedir)
                 if path.isdir(path.join(sourcedir, d))]
         if builddir_ in dirs:

commit d94c273757be71142d969bb056669d92dfede9a5

serve: fix access not associated with a value
Fixes: 8570e238eff1 ("serve: explicitely track conf.py")
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/cli/serve.py
@@ -552,7 +552,7 @@ def serve():
             files.extend(__files)
             for f in __files:
                 ctime.append(stat(f).st_mtime)
-        if path.isfile(f):
+        if path.isfile(conf_py):
             files.append(conf_py)
             ctime.append(stat(conf_py).st_mtime)
         dirs = [d for d in listdir(sourcedir)