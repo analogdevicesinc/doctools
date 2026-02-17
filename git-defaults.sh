154c01a53c239d7d34598a1ebd065acfa911a40a
Jorge Marques <jorge.marques@analog.com>
2026-02-17 13:10:15 +0100

    fixup! git-defaults: add script with git defaults

--- git-defaults.sh
+++ git-defaults.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-RUN git config --add --system user.name "CSE CI" ; \
-    git config --add --system user.email "cse-ci-notifications@analog.com" ; \
-    git config --add --system init.defaultBranch  "__runner_init_branch" ; \
-    git config --add --system advice.mergeConflict false ; \
-    git config --add --system advice.detachedHead false ; \
-    git config --add --system fetch.prune true ; \
-    git config --add --system fetch.pruneTags true ; \
-    git config --add --system safe.directory '*'
+git config --add --system user.name "CSE CI"
+git config --add --system user.email "cse-ci-notifications@analog.com"
+git config --add --system init.defaultBranch  "__runner_init_branch"
+git config --add --system advice.mergeConflict false
+git config --add --system advice.detachedHead false
+git config --add --system fetch.prune true
+git config --add --system fetch.pruneTags true
+git config --add --system safe.directory '*'
