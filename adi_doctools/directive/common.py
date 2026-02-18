analogdevicesinc/doctools

commit c828689c8e401a0e5eae2f555af8de08cef82ee0

directive: add toctree-preview
Adds a preview of the pages.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/directive/common.py
@@ -65,6 +65,15 @@ def parse_rst(state, content, uid: Optional[str] = None):
     return node
 
 
+def get_document_description(doctree):
+    for node in doctree.traverse(nodes.paragraph):
+        if node.parent.tagname != 'meta':
+            content = node.astext().strip()
+            if content:
+                return content
+    return None
+
+
 def directive_description_doctree_resolved(app, doctree, fromdocname):
     has_meta = any(
         isinstance(node, nodes.meta) and node.get('name') == 'description'
@@ -73,11 +82,7 @@ def directive_description_doctree_resolved(app, doctree, fromdocname):
     if has_meta:
         return
 
-    content = None
-    for node in doctree.traverse(nodes.paragraph):
-        if node.parent.tagname != 'meta':
-            content = node.astext().strip()
-            break
+    content = get_document_description(doctree)
     if not content:
         return
 

adi_doctools/directive/toctree_preview.py
@@ -1,0 +1,80 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from docutils import nodes
+from sphinx import addnodes
+from sphinx.directives.other import TocTree
+from sphinx.transforms.post_transforms import SphinxPostTransform
+from sphinx.util import logging
+from .common import get_document_description
+
+if TYPE_CHECKING:
+    from docutils.nodes import Node
+    from sphinx.application import Sphinx
+
+logger = logging.getLogger(__name__)
+
+
+class directive_toctree_preview(TocTree):
+    """
+    Creates a toctree, but adds a preview of the page.
+    Note that it won't show sub-levels, only depth 1.
+    """
+
+    def run(self) -> list[Node]:
+        result = super().run()
+
+        if result:
+            compound_node = result[0]
+            for node in compound_node.findall(addnodes.toctree):
+                node['include_description'] = True
+
+        return result
+
+def directive_toctree_preview_doctree_resolved(app, doctree, fromdocname):
+    # TODO monkeypatch app.env.get_and_resolve_doctree instead.
+    # We cannot ensure order
+    for toctreenode in doctree.findall(addnodes.toctree):
+        if not toctreenode.get('include_description', False):
+            continue
+
+        bullet_list = nodes.bullet_list()
+        bullet_list['classes'].append('toctree-preview')
+
+        for title_, docname in toctreenode['entries']:
+            title = app.env.titles[docname].astext()
+
+            doctree = app.env.get_doctree(docname)
+            description="<EMPTY>"
+            for node in doctree.findall(nodes.meta):
+                if node['name'] == 'description':
+                    description = node['content']
+                    break
+
+            node_list_item = nodes.list_item()
+            node_title = nodes.paragraph()
+
+            ref = nodes.reference('', '', internal=True)
+            ref['refuri'] = app.builder.get_relative_uri(
+                app.env.docname, docname
+            )
+            ref += nodes.Text(title)
+
+            node_title += ref
+            node_list_item += node_title
+
+            if description:
+                node_description = nodes.paragraph(text=description)
+                node_list_item += node_description
+
+            bullet_list += node_list_item
+
+        # Replace pending resolve_toctree with early-resolved
+        toctreenode.replace_self(bullet_list)
+
+
+def toctree_preview_setup(app: Sphinx) -> None:
+    app.add_directive('toctree-preview', directive_toctree_preview)
+
+    app.connect('doctree-resolved', directive_toctree_preview_doctree_resolved)

adi_doctools/directive/__init__.py
@@ -2,10 +2,12 @@ from .node import node_setup
 from .common import common_setup
 from .hdl import hdl_setup
 from .system_top import system_top_setup
+from .toctree_preview import toctree_preview_setup
 
 setup = [
     node_setup,
     common_setup,
     hdl_setup,
-    system_top_setup
+    system_top_setup,
+    toctree_preview_setup,
 ]

commit 0fc802eb71efc95e952ec855196320de9766ef60

directive/toctree-preview: Fix not using meta
Missed the conditional to look for the metadata tag, just
like directive_description_doctree_resolved.
Fixes: c828689c8e40 ("directive: add toctree-preview")

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/directive/common.py
@@ -76,7 +76,7 @@ def get_document_description(doctree):
 
 def directive_description_doctree_resolved(app, doctree, fromdocname):
     has_meta = any(
-        isinstance(node, nodes.meta) and node.get('name') == 'description'
+        node.get('name') == 'description'
         for node in doctree.traverse(nodes.meta)
     )
     if has_meta:

adi_doctools/directive/toctree_preview.py
@@ -45,7 +45,14 @@ def directive_toctree_preview_doctree_resolved(app, doctree, fromdocname):
             doctree = app.env.get_doctree(docname)
             # doctree_resolved may not have been yield for doctree yet,
             # so do the same call as the directive_description_doctree_resolved
-            description = get_document_description(doctree)
+
+            meta = next(
+                    (node for node in doctree.traverse(nodes.meta)
+                     if node.get('name') == 'description'),
+                    None
+                   )
+
+            description= meta['content'] if meta else get_document_description(doctree)
 
             node_list_item = nodes.list_item()
             node_title = nodes.paragraph()