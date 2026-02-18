analogdevicesinc/doctools

commit a644433b3569cfc48a525b4cfab381a1e05e7534

transforms: add lfs_to_links, mv linkcheck_sitemap
Add lfs_to_links, to convert local lfs files into links, instead of
committing all blobs. Moved from /documentation.
Also move linkcheck_sitemap ext to transforms subfolder.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/transforms/lfs.py
@@ -1,0 +1,88 @@
+from os import path, environ
+import re
+
+from sphinx.transforms.post_transforms import SphinxPostTransform
+from sphinx.util import logging
+from sphinx import addnodes
+from docutils import nodes
+
+logger = logging.getLogger(__name__)
+
+
+class lfs_to_links(SphinxPostTransform):
+    """A Sphinx post-transform that converts local paths into http for git lfs
+    artifacts.
+    Limitation: the url uses the branch, so if the image does not exist in the
+    HEAD anymore, (e.g., main), the image will return 404 for old versions.
+    A solution, that does not touch every file with image every build, is to
+    look to the history and get the sha from when it was last modified, and use
+    this commit for the url instead.
+    """
+
+    formats = ("html",)
+    default_priority = 20
+
+    def get_lfs_types(self, srcdir):
+        types_lfs = []
+        git_attr = path.join(srcdir, "..", ".gitattributes")
+        with open(git_attr, "r") as f:
+            for line in f:
+                line = line.strip()
+                if line and not line.startswith("#") and 'filter=lfs' in line:
+                    match = re.match(r"\*\.([a-zA-Z0-9]+)", line)
+                    if match:
+                        types_lfs.append('.'+match.group(1))
+        return types_lfs
+
+    def is_not_lfs(self, uri):
+        is_lfs = False
+        for ext in self.types_lfs:
+            if uri.endswith(ext):
+                is_lfs = True
+        return not is_lfs
+
+    def run(self, **kwargs) -> None:
+        self.types_lfs = self.get_lfs_types(self.document.settings.env.srcdir)
+        url = f"https://media.githubusercontent.com/media/{environ['GIT_ORG_REPOSITORY']}/{environ['GIT_BRANCH']}/docs"
+
+        docname = self.document.settings.env.docname
+        for node in self.document.findall(nodes.image):
+            uri = node['candidates'].get('*')
+            if uri:
+                if self.is_not_lfs(uri):
+                    continue
+
+                ouri = node['original_uri']
+                ouri = path.abspath(path.join(f"/{docname}", '..', ouri))
+
+                node['candidates']['?'] = f"{url}{ouri}"
+                node['original_uri'] = f"{url}{ouri}"
+                node['uri'] = f"{url}{ouri}"
+                del node['candidates']['*']
+
+        for node in self.document.findall(addnodes.download_reference):
+            targetname = node['reftarget']
+            if '://' in targetname:
+                continue
+
+            if 'filename' in node:
+                del node['filename']
+
+            if self.is_not_lfs(targetname):
+                continue
+
+            targetname = path.abspath(path.join(f"/{docname}", '..', targetname))
+            node['refuri'] = f"{url}{targetname}"
+
+
+def lfs_setup(app):
+    if "GIT_LFS_TO_LINKS" in environ:
+        if "GIT_ORG_REPOSITORY" in environ and "GIT_BRANCH" in environ:
+            app.add_post_transform(lfs_to_links)
+        else:
+            logger.error("lfs_to_links: 'GIT_LFS_TO_LINKS' in env, but 'GIT_ORG_REPOSITORY' or/and 'GIT_BRANCH' is not, skipped!")
+
+    return {
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }

adi_doctools/__init__.py
@@ -15,7 +15,7 @@ from .role import setup as role_setup
 from .lut import get_lut
 from .role.interref import interref_repos_apply, interref_repos_assert
 from .monkeypatch import monkeypatch_figure_numbers
-from .ext import setup as ext_setup
+from .transforms import setup as transforms_setup
 
 __version__ = "0.4.29"
 
@@ -182,7 +182,7 @@ def setup(app):
         setup(app)
     for setup in role_setup:
         setup(app)
-    for setup in ext_setup:
+    for setup in transforms_setup:
         setup(app)
 
     app.add_transform(unique_ids)

adi_doctools/transforms/__init__.py
@@ -1,5 +1,7 @@
 from .linkcheck_sitemap import setup as linkcheck_sitemap_setup
+from .lfs import lfs_setup
 
-setup = {
+setup = [
     linkcheck_sitemap_setup,
-}
+    lfs_setup,
+]

commit 1b25839ac642cfb0f071d40ebad2b626edcc0a30

transforms/lfs: check for original_uri existence
Sphinx does not create original_uri if not needed.
From sphinx/environment/collectors/asset.py

  if node['uri'] != original_uri:
      node['original_uri'] = original_uri

Therefore, use 'uri' if 'original_uri' not present.

Fixes: a644433b3569 ("transforms: add lfs_to_links, mv linkcheck_sitemap")

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/transforms/lfs.py
@@ -52,11 +52,17 @@ class lfs_to_links(SphinxPostTransform):
                 if self.is_not_lfs(uri):
                     continue
 
-                ouri = node['original_uri']
+                # sphinx/environment/collectors/asset.py
+                # conditionally creates original_uri
+                if 'original_uri' in node:
+                    ouri = node['original_uri']
+                else:
+                    ouri = node['uri']
                 ouri = path.abspath(path.join(f"/{docname}", '..', ouri))
 
                 node['candidates']['?'] = f"{url}{ouri}"
-                node['original_uri'] = f"{url}{ouri}"
+                if 'original_uri' in node:
+                    node['original_uri'] = f"{url}{ouri}"
                 node['uri'] = f"{url}{ouri}"
                 del node['candidates']['*']
 