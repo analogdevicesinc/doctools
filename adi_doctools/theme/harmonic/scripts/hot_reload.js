analogdevicesinc/doctools

commit cb70ef2d0a854092c94bae35a140781949bfe9e7

hot_reload: add suport to newer mermaid
It is now inside a ESM module with extra pre-defined methods. At the
extension source, was coordinated to export runMermaid to window.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/theme/harmonic/scripts/hot_reload.js
@@ -94,25 +94,29 @@ export class HotReload {
   ensure_script (value, key, map) {
     document.querySelector('head')?.append(value)
 
-    if (!key.startsWith("https://"))
-      return
-
-    if (new RegExp("^https://cdn\\.jsdelivr\\.net/npm/mathjax@[^/]+/(?:es5/)?tex-mml-chtml\\.js$").test(key)) {
-      // MathJax will apply on load, so only call if already loaded,
-      // instead of having to wait if it to be loaded to call.
-      if (typeof MathJax !== 'undefined')
-        MathJax.typeset()
-      // For reference only, if custom initialization was necessary
-      //else
-      //  value.onload = () => { console.log("MathJax loaded") }
-    } else if (new RegExp("^https://cdn\\.jsdelivr\\.net/npm/mermaid@[^/]+/dist/mermaid\\.esm\\.min\\.mjs$").test(key)) {
-      if (typeof Mermaid !== 'undefined')
-        Mermaid.run()
-      else
-        import(key).then(m => {
-          window.Mermaid = m.default
+    if (key.startsWith("https://")) {
+      if (new RegExp("^https://cdn\\.jsdelivr\\.net/npm/mathjax@[^/]+/(?:es5/)?tex-mml-chtml\\.js$").test(key)) {
+        // MathJax will apply on load, so only call if already loaded,
+        // instead of having to wait if it to be loaded to call.
+        if (typeof MathJax !== 'undefined')
+          MathJax.typeset()
+        // For reference only, if custom initialization was necessary
+        //else
+        //  value.onload = () => { console.log("MathJax loaded") }
+      } else if (new RegExp("^https://cdn\\.jsdelivr\\.net/npm/mermaid@[^/]+/dist/mermaid\\.esm\\.min\\.mjs$").test(key)) {
+        if (typeof Mermaid !== 'undefined')
           Mermaid.run()
-        })
+        else
+          import(key).then(m => {
+            window.Mermaid = m.default
+            Mermaid.run()
+          })
+      }
+    } else {
+      if (new RegExp("^import mermaid from \"https://cdn\\.jsdelivr\\.net/npm/mermaid@[^/]+/dist/mermaid\\.esm\\.min\\.mjs\";").test(key)) {
+        if (typeof runMermaid !== 'undefined')
+          runMermaid(true)
+      }
     }
   }
   get_script_key (script) {

commit e87c024085a290e6bdc28fe87d04688a43c93aca

fix: hot_reload: add suport to newer mermaid
At appending to the header, it is required to one-shot call mermaid to
render at the current, hot-reloaded page.
Fixes: cb70ef2d0a85 ("hot_reload: add support to newer mermaid")

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/theme/harmonic/scripts/hot_reload.js
@@ -114,8 +114,19 @@ export class HotReload {
       }
     } else {
       if (new RegExp("^import mermaid from \"https://cdn\\.jsdelivr\\.net/npm/mermaid@[^/]+/dist/mermaid\\.esm\\.min\\.mjs\";").test(key)) {
-        if (typeof runMermaid !== 'undefined')
+        if (typeof runMermaid !== 'undefined') {
           runMermaid(true)
+        } else {
+          // Needs custom initialization, use one-shot setter
+          Object.defineProperty(window, 'runMermaid', {
+            configurable: true,
+            set: f => {
+              delete window.runMermaid
+              window.runMermaid = f
+              f(true)
+            }
+          })
+        }
       }
     }
   }