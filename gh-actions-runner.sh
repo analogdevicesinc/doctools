analogdevicesinc/doctools

commit 560a6d09f11de046e5c6eff9b0893915b63a1b69

gh-actions-runner: bump version
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

gh-actions-runner.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-runner_version=2.332.0
-runner_version_sha=f2094522a6b9afeab07ffb586d1eb3f190b6457074282796c497ce7dce9e0f2a
-node20_version=20.20.1
-node20_version_sha=6362e50804cdcc110592201f67beda93bcd702fdcbe1c42840a50d590e3af0ce
+runner_version=2.333.0
+runner_version_sha=7ce6b3fd8f879797fcc252c2918a23e14a233413dc6e6ab8e0ba8768b5d54475
 node24_version=24.14.0
 node24_version_sha=dbf5b8665dec15e59e6359a517fefb47b23fdb9152d8def975b9bca3dfc6d355
 k8s_version=0.8.1
@@ -16,18 +14,21 @@ curl -o actions-runner.tar.gz -L https://github.com/actions/runner/releases/down
     tar xzf actions-runner.tar.gz && rm actions-runner.tar.gz
 
 rm -r ./externals/node*
-curl -o node20.tar.gz -L https://nodejs.org/dist/v${node20_version}/node-v${node20_version}-linux-x64.tar.gz && \
-    echo "${node20_version_sha} node20.tar.gz" | sha256sum -c && \
-    tar xzf node20.tar.gz && \
-    mv node-v${node20_version}-linux-x64 ./externals/node20 && \
-    rm node20.tar.gz
+
+if [[ "$FORCE_JAVASCRIPT_ACTIONS_TO_NODE24" != "true" ]]; then
+    curl -o node20.tar.gz -L https://nodejs.org/dist/v${node20_version}/node-v${node20_version}-linux-x64.tar.gz && \
+        echo "${node20_version_sha} node20.tar.gz" | sha256sum -c && \
+        tar xzf node20.tar.gz && \
+        mv node-v${node20_version}-linux-x64 ./externals/node20 && \
+        rm node20.tar.gz
+    ./externals/node20/bin/node ./externals/node20/lib/node_modules/npm/bin/npm-cli.js --prefix ./externals/node20 -g update
+fi
 
 curl -o node24.tar.gz -L https://nodejs.org/dist/v${node24_version}/node-v${node24_version}-linux-x64.tar.gz && \
     echo "${node24_version_sha} node24.tar.gz" | sha256sum -c && \
     tar xzf node24.tar.gz && \
     mv node-v${node24_version}-linux-x64 ./externals/node24 && \
     rm node24.tar.gz
-./externals/node20/bin/node ./externals/node20/lib/node_modules/npm/bin/npm-cli.js --prefix ./externals/node20 -g update
 ./externals/node24/bin/node ./externals/node24/lib/node_modules/npm/bin/npm-cli.js --prefix ./externals/node24 -g update
 rm -r ~/.npm
 curl -o actions-runner-k8s.zip -L https://github.com/actions/runner-container-hooks/releases/download/v${k8s_version}/actions-runner-hooks-k8s-${k8s_version}.zip && \

commit 78e5bbddcb70b15d6ca015995f78aca60e0c61c9

fix: gh-actions-runner: missing node20
Also bump to latest 20 version.
Fixes: 560a6d09f11d ("gh-actions-runner: bump version")

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

gh-actions-runner.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-set -e
+set -eo
 
 runner_version=2.333.0
 runner_version_sha=7ce6b3fd8f879797fcc252c2918a23e14a233413dc6e6ab8e0ba8768b5d54475
+node20_version=20.20.2
+node20_version_sha=19e56f0825510207dd904f087fe52faa0a4eb6b2aab5f0ea7a33830d04888b8b
 node24_version=24.14.0
 node24_version_sha=dbf5b8665dec15e59e6359a517fefb47b23fdb9152d8def975b9bca3dfc6d355
 k8s_version=0.8.1