799478e43d2a92e5965597f72d379b88fc78694e
Jorge Marques <jorge.marques@analog.com>
2026-04-08 00:19:57 +0200

    llm: remove dead code
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- llm/fmt_args.py
+++ llm/fmt_args.py
@@ -10,8 +10,6 @@ def fmt_args(text: str) -> str:
         return environ.get(var, "")
     return pattern.sub(repl, text)
 
-    content = fmt_args(content_)
-
 if __name__ == "__main__":
     with open(sys.argv[1], 'r') as f:
         content = f.read()
