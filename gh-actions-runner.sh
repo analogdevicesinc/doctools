27e116b9b368fbc1fb8b142f2ef480e235f433ef
Jorge Marques <jorge.marques@analog.com>
2026-04-20 16:42:29 +0200

    gh-actions-runner: bump versions
    And make easier to extract the current SHA.
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- gh-actions-runner.sh
+++ gh-actions-runner.sh
@@ -2,16 +2,17 @@
 
 set -e
 
-runner_version=2.333.0
-runner_version_sha=7ce6b3fd8f879797fcc252c2918a23e14a233413dc6e6ab8e0ba8768b5d54475
+runner_version=2.333.1
+runner_version_sha=18f8f68ed1892854ff2ab1bab4fcaa2f5abeedc98093b6cb13638991725cab74
 node20_version=20.20.2
 node20_version_sha=19e56f0825510207dd904f087fe52faa0a4eb6b2aab5f0ea7a33830d04888b8b
-node24_version=24.14.0
-node24_version_sha=dbf5b8665dec15e59e6359a517fefb47b23fdb9152d8def975b9bca3dfc6d355
+node24_version=24.15.0
+node24_version_sha=44836872d9aec49f1e6b52a9a922872db9a2b02d235a616a5681b6a85fec8d89
 k8s_version=0.8.1
 k8s_version_sha=2c3c4526f8a8d517373a2a0daa6122d7887bdc465c54945c6d716db87d9d9b4f
 
 curl -o actions-runner.tar.gz -L https://github.com/actions/runner/releases/download/v${runner_version}/actions-runner-linux-x64-${runner_version}.tar.gz && \
+    sha256sum actions-runner.tar.gz && \
     echo "${runner_version_sha} actions-runner.tar.gz" | sha256sum -c && \
     tar xzf actions-runner.tar.gz && rm actions-runner.tar.gz
 
@@ -19,6 +20,7 @@ rm -r ./externals/node*
 
 if [[ "$FORCE_JAVASCRIPT_ACTIONS_TO_NODE24" != "true" ]]; then
     curl -o node20.tar.gz -L https://nodejs.org/dist/v${node20_version}/node-v${node20_version}-linux-x64.tar.gz && \
+        sha256sum node20.tar.gz && \
         echo "${node20_version_sha} node20.tar.gz" | sha256sum -c
     tar xzf node20.tar.gz && \
         mv node-v${node20_version}-linux-x64 ./externals/node20 && \
@@ -27,6 +29,7 @@ if [[ "$FORCE_JAVASCRIPT_ACTIONS_TO_NODE24" != "true" ]]; then
 fi
 
 curl -o node24.tar.gz -L https://nodejs.org/dist/v${node24_version}/node-v${node24_version}-linux-x64.tar.gz && \
+    sha256sum node24.tar.gz && \
     echo "${node24_version_sha} node24.tar.gz" | sha256sum -c
 tar xzf node24.tar.gz && \
     mv node-v${node24_version}-linux-x64 ./externals/node24 && \
@@ -34,5 +37,6 @@ tar xzf node24.tar.gz && \
 ./externals/node24/bin/node ./externals/node24/lib/node_modules/npm/bin/npm-cli.js --prefix ./externals/node24 -g update
 rm -r ~/.npm
 curl -o actions-runner-k8s.zip -L https://github.com/actions/runner-container-hooks/releases/download/v${k8s_version}/actions-runner-hooks-k8s-${k8s_version}.zip && \
+    sha256sum actions-runner-k8s.zip && \
     echo "${k8s_version_sha} actions-runner-k8s.zip" | sha256sum -c
 unzip actions-runner-k8s.zip && rm actions-runner-k8s.zip
