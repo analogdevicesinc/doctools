50d9a42b46ef9c47b1f8f2198ff51476b7ff3aaa
Jorge Marques <jorge.marques@analog.com>
2026-06-24 22:20:15 +0200

    fetch-fonts: add jetbrains mono
    Harmonic guidelines monofont:
    https://harmonic.app.analog.com/1816b7bc5/p/431143-typography
    
    Signed-off-by: Jorge Marques <jorge.marques@analog.com>

--- ci/fetch-fonts.sh
+++ ci/fetch-fonts.sh
@@ -6,23 +6,30 @@ format=${2:-woff2}
 declare -A url
 url[barlow]="https://github.com/jpt/barlow/archive/refs/tags/1.422.tar.gz"
 url[inter]="https://github.com/rsms/inter/releases/download/v4.0/Inter-4.0.zip"
+url[jetbrains-mono]="https://github.com/JetBrains/JetBrainsMono/releases/download/v2.304/JetBrainsMono-2.304.zip"
 
 declare -A sha
 sha[barlow]="f4bc7fd2802844deead4d19634b9c4d11710624a48fcfd7196821e353159c048"
 sha[inter]="ff970a5d4561a04f102a7cb781adbd6ac4e9b6c460914c7a101f15acb7f7d1a4"
+sha[jetbrains-mono]="6f6376c6ed2960ea8a963cd7387ec9d76e3f629125bc33d1fdcd7eb7012f7bbf"
 
 declare -A dir
 dir[inter:woff2]="web"
 dir[inter:ttf]="extras/ttf"
 dir[inter]=${dir[inter:$format]}
+dir[jetbrains-mono:woff2]="webfonts"
+dir[jetbrains-mono:ttf]="ttf"
+dir[jetbrains-mono]=${dir[jetbrains-mono:$format]}
 
 declare -A files
 files[barlow]="fonts/$format/Barlow-Medium.$format fonts/$format/Barlow-SemiBold.$format OFL.txt"
 files[inter]="${dir[inter]}/Inter-Regular.$format ${dir[inter]}/Inter-SemiBold.$format LICENSE.txt"
+files[jetbrains-mono]="fonts/${dir[jetbrains-mono]}/JetBrainsMono-Regular.$format fonts/${dir[jetbrains-mono]}/JetBrainsMono-Bold.$format OFL.txt"
 
 declare -A ext
 ext[barlow]="tar.gz"
 ext[inter]="zip"
+ext[jetbrains-mono]="zip"
 
 if [ -d .cache-fonts ]; then
 	rm -r .cache-fonts
