analogdevicesinc/doctools

commit 1a839611fed388219509af38b74ce139a78c6442

.github/build-doc: use dirhtml
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/cli/serve.py
@@ -153,7 +153,7 @@ class Serve:
                 override_args.extend(["-D", f"{key}={value}"])
 
         build_args = [
-            "sphinx-build", "-b", "html",
+            "sphinx-build", "-b", "dirhtml",
             srcdir_rel, builddir_rel,
             "-d", doctreedir_rel,
             "-j", "auto"
@@ -220,7 +220,9 @@ cwd = {cwd_display}""")
 
         if self.builder == 'pdf':
             self.builder = 'latexpdf'
-        if self.builder not in ['html', 'latex', 'latexpdf']:
+        elif self.builder == 'html':
+            self.builder = 'dirhtml'
+        if self.builder not in ['dirhtml', 'latex', 'latexpdf']:
             logger.error(log['builder'].format(self.builder))
             return True
 
@@ -682,7 +684,7 @@ cwd = {cwd_display}""")
             def log_message(_self, format, *args):
                 return
 
-        if not self.once and self.builder == "html":
+        if not self.once and self.builder == "dirhtml":
             try:
                 socketserver.ThreadingTCPServer.allow_reuse_address = True
                 self.http_p = socketserver.ThreadingTCPServer(("", self.port), Handler)
@@ -761,7 +763,7 @@ cwd = {cwd_display}""")
                           parallel=0, status=sys.stdout if self.verbose else None,
                           warning=warning_stream)
 
-        if self.builder == "html":
+        if self.builder == "dirhtml":
             server_url = f"http://127.0.0.1:{self.port}?v={str(uuid4())[:2]}"
             if self.jsonrpc:
                 notify("server/started", {"port": self.port, "url": server_url})
@@ -780,7 +782,7 @@ cwd = {cwd_display}""")
                 with open(dev_pool, 'w') as dev_f:
                     dev_f.write(dev_pool_val_)
 
-        if self.builder == "html":
+        if self.builder == "dirhtml":
             update_dev_pool("")
 
         def get_doc_sources_included():
@@ -843,7 +845,11 @@ cwd = {cwd_display}""")
                 c = -3
             else:
                 return trigger_rst_
-            path_ = path.relpath(file, directory)[:c] + ".html"
+            path_ = path.relpath(file, directory)[:c]
+            if path_.endswith("index"):
+                path_ = path_ + ".html"
+            else:
+                path_ = path.join(path_, "index.html")
             if path_.startswith("../"):
                 return trigger_rst_
             return (file, path_)
@@ -959,7 +965,7 @@ cwd = {cwd_display}""")
                         toctree_changed = True
                     toctree_content = _toctree_content
 
-            if self.builder == "html":
+            if self.builder == "dirhtml":
                 if update_sphinx:
                     message = ""
                     if trigger_rst[0] == '':

.github/workflows/build-doc.yml
@@ -36,20 +36,20 @@ jobs:
       run: |
         source ../venv/3/build-doc-latest/bin/activate
         ADOC_DOC_VERSION=${{ inputs.tag }} \
-          make html SPHINXOPTS='-W --keep-going'
-        rm -rf _build/html/_sources
+          make dirhtml SPHINXOPTS='-W --keep-going'
+        rm -rf _build/dirhtml/_sources
 
     - name: Add extra sources
       working-directory: docs
       run: |
         curl -sL -H "Authorization: Bearer ${{ secrets.GITHUB_TOKEN }}" \
-               https://raw.githubusercontent.com/analogdevicesinc/doctools/refs/heads/vector/index.js -o _build/html/_static/turboquant-wasm.js
+               https://raw.githubusercontent.com/analogdevicesinc/doctools/refs/heads/vector/index.js -o _build/dirhtml/_static/turboquant-wasm.js
 
     - name: Store the generated doc
       uses: actions/upload-artifact@v7
       with:
         name: html
-        path: docs/_build/html
+        path: docs/_build/dirhtml
 
   build-doc-min:
     runs-on: ${{ vars.RUNNER != '' && fromJSON(vars.RUNNER) || 'ubuntu-latest' }}
@@ -82,14 +82,14 @@ jobs:
       working-directory: docs
       run: |
         source ../venv/3.8/build-doc-min/bin/activate
-        make clean html SPHINXOPTS='-W --keep-going'
-        rm -rf _build/html/_sources
+        make clean dirhtml SPHINXOPTS='-W --keep-going'
+        rm -rf _build/dirhtml/_sources
 
     - name: Store the generated doc
       uses: actions/upload-artifact@v7
       with:
         name: html-min
-        path: docs/_build/html
+        path: docs/_build/dirhtml
 
   check-doc:
     runs-on: ${{ vars.RUNNER != '' && fromJSON(vars.RUNNER) || 'ubuntu-latest' }}

commit 92b5286929bb33d2b403353aee2e08968bf2fa01

dev-poll: strip index.html to match toctree
Fixes: 1a839611fed3 (".github/build-doc: use dirhtml")
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/cli/serve.py
@@ -971,7 +971,7 @@ cwd = {cwd_display}""")
                     if trigger_rst[0] == '':
                         pass
                     elif path.isfile(path.join(self.builddir, trigger_rst[1])):
-                        message += f"@docname {trigger_rst[1]}\n"
+                        message += f"@docname {trigger_rst[1][:-len('index.html')]}\n"
                     else:
                         logger.warning(log['doc_not_built'].format(trigger_rst[0]))
                     if toctree_changed: