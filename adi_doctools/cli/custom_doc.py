analogdevicesinc/doctools

commit 700a87159e894ba40fca4cd7aa6822d19bff9f9f

fix: custom-doc: improper copying image pattern
Was matching a single whitespace, but < 10 would have 2.

Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/cli/custom_doc.py
@@ -802,7 +802,7 @@ def parse_warnings(doc_dir):
 def parse_status(doc_dir):
     statusfile = path.join(doc_dir, '..', 'status.txt')
 
-    re_images = r"^copying images\.\.\.\s+\[\s?\d+%\]\s(.*)$"
+    re_images = r"^copying images\.\.\.\s+\[\s+?\d+%\]\s(.*)$"
     status = open(statusfile, "r")
     for e in status:
         e = e.replace("[33m", "").replace("[39;49;00m", "").replace("[01m", "")

commit a1ca68812bb62adf16401d4981d595c8f5fcc81a

fix: custom_doc: match [100%] from Sphinx log
\s+? would exclude [100%], that could happen if 2 files are copied, for
example (50% -> 100%).

Fixes: 700a87159e89 ("fix: custom-doc: improper copying image pattern")
Signed-off-by: Jorge Marques <jorge.marques@analog.com>

adi_doctools/cli/custom_doc.py
@@ -868,7 +868,7 @@ def parse_warnings(doc_dir):
 def parse_status(doc_dir):
     statusfile = path.join(doc_dir, '..', 'status.txt')
 
-    re_images = r"^copying images\.\.\.\s+\[\s+?\d+%\]\s(.*)$"
+    re_images = r"^copying images\.\.\.\s+\[\s*\d+%\]\s(.*)$"
     with open(statusfile, "r") as status:
         for e in status:
             e = e.replace("\x1b[33m", "").replace("\x1b[39;49;00m", "").replace("\x1b[01m", "")